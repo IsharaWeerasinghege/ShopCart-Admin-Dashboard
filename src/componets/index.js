7e40ed056e01753468255b7f54d03499b2aec0bb
WSishara96 <surangaishara8@gmail.com>
2023-02-14 20:46:34 +0530

    Dashboard v.1.0

---
+++ src/componets/index.js
@@ -0,0 +1,18 @@
+export {default as Button} from './Button';
+export { default as ThemeSettings } from './ThemeSettings';
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Cart } from './Cart';
+export { default as Chat } from './Chat';
+export { default as Notification } from './Notification';
+export { default as UserProfile } from './UserProfile';
+export { default as SparkLine } from './charts/SparkLine';
+export { default as LineChart } from './charts/LineChart';
+export { default as Stacked } from './charts/Stacked';
+export { default as Pie } from './charts/Pie';
+export { default as ChartHeader } from './ChartHeader';
+export { default as Header } from './Header';
+
+
+
---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,37 @@
+import React, {createContext, useContext, useState} from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({children}) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+    const handleClick = (clicked) => setIsClicked({...initialState, [clicked]: true});
+
+    return (
+        <StateContext.Provider
+            value={
+                {
+                    activeMenu,
+                    setActiveMenu,
+                    handleClick,
+                    isClicked,
+                    setIsClicked,
+                    screenSize,
+                    setScreenSize
+                }
+            }
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
